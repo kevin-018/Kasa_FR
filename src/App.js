d7b62fd41bd801caaca41ee0e913a5e498c04a5c
Kevin Fouquet <kevinfouquet18@gmail.com>
2024-03-03 16:57:20 +0100

    integration of routers and different routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Accueil from "./Kasa/Home/Accueil";
+import Apropos from "./Kasa/Services/Apropos";
+import Logements from "./Kasa/Logements/Logement";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Accueil />} />
+        <Route path="/A_propos" element={<Apropos />} />
+        <Route path="/Logements" element={<Logements />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/Kasa/Home/Accueil.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+
+
+export default function Accueil() {
+  return (
+    <div>
+      <h1> Accueil </h1>
+      <button>Accueil</button>
+      <button>A Propos</button>
+    </div>
+  );
+}
